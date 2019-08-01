c0681b06d3b0689155825b895d3828adc3cf0aed
Miranda <mirandarregan@gmail.com>
2019-08-01 11:08:34 -0400

    Pulls from API and prints names.

--- lib/code_day_project_web/views/page_view.ex
+++ lib/code_day_project_web/views/page_view.ex
@@ -2,7 +2,25 @@ defmodule CodeDayProjectWeb.PageView do
   use CodeDayProjectWeb, :view
 
   def getAPI() do
-    Poison.decode(HTTPoison.get!("https://api.github.com/orgs/octokit/repos"))
-    # Poison.decode(response)
+    apiStuff = HTTPoison.get!("https://api.github.com/orgs/github/repos?type=public")
+    decodedAPI = Poison.decode!(apiStuff.body)
+    names = Enum.map(decodedAPI, fn x -> x["name"] end)
+    
+    # %HTTPoison.Response{status_code: 200,
+    #                 headers: [{"content-type", "application/json"}],
+    #                 body: "{...}"}
   end
+
+
+  
 end
+
+# defmodule GitHub do
+#   use HTTPoison.Base
+
+#   @endpoint "https://api.github.com"
+
+#   def process_url(url) do
+#     @endpoint <> url
+#   end
+# end
