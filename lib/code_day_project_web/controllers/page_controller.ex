8a38135a6cc0a73eaac8a0033bb63a667333b851
Miranda <mirandarregan@gmail.com>
2019-08-01 15:41:06 -0400

    Submitting a organization name to the form returns list of repo names

--- lib/code_day_project_web/controllers/page_controller.ex
+++ lib/code_day_project_web/controllers/page_controller.ex
@@ -2,10 +2,18 @@ defmodule CodeDayProjectWeb.PageController do
   use CodeDayProjectWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    content = []
+    render(conn, "index.html", content: content)
   end
 
   def show(conn, _params) do
-    render(conn, "index.html")
+    content = conn.params
+    organizationName = Map.fetch!(content, "organization")
+    response = HTTPoison.get!("https://api.github.com/orgs/#{organizationName}/repos?type=public")
+    decodedAPIMap = Poison.decode!(response.body)
+    repos = Enum.map(decodedAPIMap, fn x -> x["name"] end)
+    render(conn, "index.html", content: repos)
   end
+
+
 end
