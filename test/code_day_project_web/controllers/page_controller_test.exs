9f813b2ba15e6ad4741807bd3d595833e32e0675
Miranda <mirandarregan@gmail.com>
2019-08-02 19:43:44 -0400

    Test added for return number of repos found and passing

--- test/code_day_project_web/controllers/page_controller_test.exs
+++ test/code_day_project_web/controllers/page_controller_test.exs
@@ -24,6 +24,24 @@ defmodule CodeDayProjectWeb.PageControllerTest do
     assert html_response(conn, 200) =~ "pong"
   end
 
+  test "POST /findOrg returns correct number of names in map", %{conn: conn} do
+
+    bypass = Bypass.open()
+
+    fakeMap = [%{"name" => "pong"}]
+    jsonFakeMap = Poison.encode!(fakeMap)
+
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, jsonFakeMap)
+    end)
+
+    Application.put_env(:code_day_project, :github_api, "localhost:#{bypass.port}")
+    conn = Plug.Conn.put_req_header(conn, "content-type", "application/json")
+    |> post("/findOrg", "{\"organization\": \"github\"}")
+    
+    assert html_response(conn, 200) =~ "Repositories found: 1"
+  end
+
   test "POST /findOrg returns nothing found when nothing found", %{conn: conn} do
 
     bypass = Bypass.open()
