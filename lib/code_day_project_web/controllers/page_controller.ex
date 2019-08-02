873ea79009561dd13788af598e7de340009c291f
Miranda <mirandarregan@gmail.com>
2019-08-02 19:23:51 -0400

    Added test for 404 error handling and passing.

--- lib/code_day_project_web/controllers/page_controller.ex
+++ lib/code_day_project_web/controllers/page_controller.ex
@@ -18,11 +18,14 @@ defmodule CodeDayProjectWeb.PageController do
 
     url = Application.get_env(:code_day_project, :github_api)
     
+    with %HTTPoison.Response{status_code: 200} <- HTTPoison.get!("#{url}/orgs/#{organizationName}/repos?type=public") do
     response = HTTPoison.get!("#{url}/orgs/#{organizationName}/repos?type=public") 
-    
     Poison.decode!(response.body)
     |> Enum.map(fn x -> x["name"] end) 
-
+    else %HTTPoison.Response{status_code: 404} -> ["Nothing was found"]
+          _ -> ["Something went wrong"]
+    end
+    
   end
 
 end
--- test/code_day_project_web/controllers/page_controller_test.exs
+++ test/code_day_project_web/controllers/page_controller_test.exs
@@ -6,7 +6,7 @@ defmodule CodeDayProjectWeb.PageControllerTest do
     assert html_response(conn, 200) =~ "Code Day Project for ScriptDrop"
   end
 
-  test "POST /findOrg", %{conn: conn} do
+  test "POST /findOrg returns name(s) of repos", %{conn: conn} do
 
     bypass = Bypass.open()
 
@@ -22,6 +22,21 @@ defmodule CodeDayProjectWeb.PageControllerTest do
     |> post("/findOrg", "{\"organization\": \"github\"}")
     
     assert html_response(conn, 200) =~ "pong"
+  end
+
+  test "POST /findOrg returns error when nothing found", %{conn: conn} do
+
+    bypass = Bypass.open()
+
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 404, "Resource not found")
+    end)
+
+    Application.put_env(:code_day_project, :github_api, "localhost:#{bypass.port}") |> IO.inspect
+    conn = Plug.Conn.put_req_header(conn, "content-type", "application/json")
+    |> post("/findOrg", "{\"organization\": \"notAOrg\"}")
+    
+    assert html_response(conn, 200) =~ "Nothing was found"
   end
 
 end
