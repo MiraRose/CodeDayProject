7ad7ba4e4347815bcacbe211016c7412ce468177
Miranda <mirandarregan@gmail.com>
2019-08-02 14:52:36 -0400

    Refactoring on page_controller

--- lib/code_day_project_web/controllers/page_controller.ex
+++ lib/code_day_project_web/controllers/page_controller.ex
@@ -2,19 +2,27 @@ defmodule CodeDayProjectWeb.PageController do
   use CodeDayProjectWeb, :controller
 
   def index(conn, _params) do
-    content = []
-    render(conn, "index.html", content: content)
+    
+    render(conn, "index.html", content: [])
   end
 
-  def show(conn, _params) do
-    url = Application.get_env(:code_day_project, :github_api)
-    content = conn.params 
-    organizationName = Map.fetch!(content, "organization")
-    response = HTTPoison.get!("#{url}/orgs/#{organizationName}/repos?type=public") 
-    decodedAPIMap = Poison.decode!(response.body)
-    repos = Enum.map(decodedAPIMap, fn x -> x["name"] end) 
+  def show(conn, _params) do 
+    
+    repos = Map.fetch!(conn.params, "organization")
+    |> getListOfRepoNames()
+
     render(conn, "index.html", content: repos)
   end
 
+  def getListOfRepoNames(organizationName) do
+
+    url = Application.get_env(:code_day_project, :github_api)
+    
+    response = HTTPoison.get!("#{url}/orgs/#{organizationName}/repos?type=public") 
+    
+    Poison.decode!(response.body)
+    |> Enum.map(fn x -> x["name"] end) 
+
+  end
 
 end
